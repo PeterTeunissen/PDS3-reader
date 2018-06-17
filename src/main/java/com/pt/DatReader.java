53d2462f3c23f9024869e3692571a6b8168d2d78
Peter Teunissen <Peter.Teunissen@cerner.com>
2018-06-16 22:27:31 -0400

    Add wildcard input support and a third parameter to suppress the file content to the screen.

--- src/main/java/com/pt/DatReader.java
+++ src/main/java/com/pt/DatReader.java
@@ -3,6 +3,10 @@ package com.pt;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -21,21 +25,51 @@ public class DatReader {
 		try {
 
 			if (args.length < 2) {
-				System.out.println("Use two parameters: <filename> <showheadersflag>");
-				System.out.println("E.g. JAD_HSK_ELC_ALL_2014020_V02.LBL true");
+				System.out.println("Use two or three parameters: <filename> <showHeadersFlag> <showFileOnScreen>");
+				System.out.println("The showHeadersFlag parameter should be true or false");
+				System.out.println("The showFileOnScreen parameter is optional, can be true or false");
+				System.out.println("E.g. JAD_HSK_ELC_ALL_2014020_V02.LBL true true");
 				System.out.println("E.g. JAD_HSK_ELC_ALL_2014020_V02.LBL false");
 				System.exit(0);
 			}
 
+			boolean showHeaders = Boolean.valueOf(args[1]);
+
+			boolean showFileOnScreen = false;
+			if (args.length > 1) {
+				showFileOnScreen = Boolean.valueOf(args[2]);
+			}
+
+			boolean onScreen = showFileOnScreen;
+
+			File f = new File(args[0]);
+			String parent = f.getParent();
+			if (parent == null) {
+				parent = ".";
+			}
+
+			DatReader reader = new DatReader();
+			try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(parent), f.getName())) {
+				dirStream.forEach(path -> reader.processFile(path.toString(), showHeaders, onScreen));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void processFile(String file, boolean hdr, boolean toSysOut) {
+
+		try {
 			ColDefReader rdr = new ColDefReader();
-			FileDescriptor fd = rdr.readDefinition(args[0]);
+			FileDescriptor fd = rdr.readDefinition(file);
 
 			System.out.println(fd);
 
 			FileReader frdr = new FileReader();
 			List<RowData> filedata = frdr.readFile(fd.datFile, fd);
-
-			boolean hdr = Boolean.parseBoolean(args[1]);
 
 			File outF = new File(fd.csvFile);
 			if (outF.exists()) {
@@ -60,7 +94,9 @@ public class DatReader {
 						first = false;
 						bf.append(cd.colName);
 					}
-					System.out.println(bf.toString());
+					if (toSysOut) {
+						System.out.println(bf.toString());
+					}
 					bf.append("\n");
 					writer.write(bf.toString());
 				}
@@ -73,7 +109,9 @@ public class DatReader {
 					first = false;
 					bf.append(cd.colValue);
 				}
-				System.out.println(bf.toString());
+				if (toSysOut) {
+					System.out.println(bf.toString());
+				}
 				bf.append("\n");
 				writer.write(bf.toString());
 			}
